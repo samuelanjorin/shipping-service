8e144c28856a997c32b085a9a18b4630e3f0ab60
Samuel Anjorin <samuel.anjorin12@gmail.com>
2019-06-02 11:12:33 +0100

    added migration

---
+++ migrations/insertRecords.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO `shipping_region` (`shipping_region_id`, `shipping_region`) VALUES
+       (1, 'Please Select') , (2, 'US / Canada'),
+       (3, 'Europe'),         (4, 'Rest of World');
+
+-- Populate shipping table
+INSERT INTO `shipping` (`shipping_id`,   `shipping_type`,
+                        `shipping_cost`, `shipping_region_id`) VALUES
+       (1, 'Next Day Delivery ($20)', 20.00, 2),
+       (2, '3-4 Days ($10)',          10.00, 2),
+       (3, '7 Days ($5)',              5.00, 2),
+       (4, 'By air (7 days, $25)',    25.00, 3),
+       (5, 'By sea (28 days, $10)',   10.00, 3),
+       (6, 'By air (10 days, $35)',   35.00, 4),
+       (7, 'By sea (28 days, $30)',   30.00, 4);
